e2b7d259e8b44eb15153d47ef82e21f40e5a97ff
Korawit Prutsachainimmit <korawit.p@gmail.com>
2022-04-11 11:03:36 +0700

    Add sign in UI

--- src/main.js
+++ src/main.js
@@ -2,6 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIgexMZQmfqh2YGcEd_1dvqwpvY2xiYwM",
+  authDomain: "vue-pwa-firebase-demo.firebaseapp.com",
+  projectId: "vue-pwa-firebase-demo",
+  storageBucket: "vue-pwa-firebase-demo.appspot.com",
+  messagingSenderId: "496738355926",
+  appId: "1:496738355926:web:60c3ee485fc314be055777",
+  measurementId: "G-CY3WG4YGKP"
+};
+
+firebase.initializeApp(firebaseConfig)
+Vue.prototype.$firebase = firebase
+console.log("Firebase initializing ... done");
 
 Vue.config.productionTip = false
 
--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,5 +1,57 @@
 <template>
-  <h3>Sign In</h3>
+  <v-app>
+    <v-main>
+      <v-form>
+        <v-container align="center" align-self="center" pa-10>
+          <v-row>
+            <v-col>
+              <v-img src="@/assets/house.png" contain max-height="150">
+              </v-img>
+            </v-col>
+          </v-row>
+          <v-row class="mb-5">
+            <v-col align="center">
+              <h2>InstaHouse</h2>
+            </v-col>
+          </v-row>
+          <v-row no-gutters>
+            <v-col cols="12">
+              <v-text-field
+                v-model="email"
+                name="email"
+                label="Email"
+                id="email"
+                hint="Your email address"
+                outlined
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                v-model="password"
+                type="password"
+                name="password"
+                label="Password"
+                id="password"
+                hint="Your password"
+                outlined
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col align="center">
+              <v-btn rounded large color="primary" @click="signInWithPassword">Sign In with Password</v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
+    </v-main>
+    <v-snackbar v-model="gotError">
+      {{ errorMessage }}
+      <template v-slot:action="{ attrs }">
+        <v-btn color="pink" text v-bind="attrs"> Close </v-btn>
+      </template>
+    </v-snackbar>
+  </v-app>
 </template>
 
 <script>
