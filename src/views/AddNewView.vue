4e297d4fab1ffaf9115285a6ea0772877138f8d1
Korawit Prutsachainimmit <korawit.p@gmail.com>
2022-04-11 14:52:55 +0700

    Add new post

--- src/views/AddNewView.vue
+++ src/views/AddNewView.vue
@@ -16,7 +16,7 @@
       <v-col cols="12" class="mt-5">
         <v-text-field
           name="name"
-          v-model="name"
+          v-model="title"
           label="Name of property"
           id="id"
           hint="Ex. Townhome 2 floor in downtown"
@@ -27,9 +27,19 @@
       <v-col cols="12">
         <v-text-field
           name="description"
-          v-model="description"
+          v-model="subtitle"
           label="Description of property"
           hint="Ex. 250 sqrm. 3 bedroom 2 bathroom"
+          id="id"
+          outlined
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          name="price"
+          v-model="price"
+          label="Price of property"
+          hint="10000000"
           id="id"
           outlined
         ></v-text-field>
@@ -47,10 +57,60 @@
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      overlay: false,
+      userId: "",
+      photoUrl:
+        "https://firebasestorage.googleapis.com/v0/b/fir-44bed.appspot.com/o/photos%2Fpicture.png?alt=media&token=4f59144a-44a8-47c9-9188-e885f8d07ac0",
+      title: "",
+      subtitle: "",
+      price: "",
+    };
+  },
+  methods: {
+    uploadPhoto() {
+      // Upload to Firebase storage
+      this.overlay = true;
+      const filename = `${Date.now()}.png`;
+      const storageRef = this.$firebase.storage().ref("photos");
+      const newFileRef = storageRef.child(filename);
+      newFileRef.put(this.$refs.file.files[0]).then((snapshot) => {
+        console.log("Uploaded a blob or file!");
+        snapshot.ref.getDownloadURL().then((downloadURL) => {
+          console.log("File available at", downloadURL);
+          this.photoUrl = downloadURL;
+          this.overlay = false;
+        });
+      });
+    },
+    selectPhoto() {
+      // Call input file to select a photo
+      this.$refs.file.click();
+    },
+    savePost() {
+      // Add a new document with a generated id.
+      const db = this.$firebase.firestore();
+      const userId = this.$firebase.auth().currentUser.email;
+      db.collection("posts")
+        .add({
+          userId: userId,
+          photoUrl: this.photoUrl,         
+          title: this.title,
+          subtitle: this.subtitle,
+          price: this.price,
+        })
+        .then((docRef) => {
+          console.log("Document written with ID: ", docRef.id);
+          this.$router.replace("/")
+        })
+        .catch((error) => {
+          console.error("Error adding document: ", error);
+        });
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
