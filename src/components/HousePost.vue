992d856bd02ae1d054a62d0dc423db9cd62af50a
Korawit Prutsachainimmit <korawit.p@gmail.com>
2022-04-11 14:03:26 +0700

    Add feed display

---
+++ src/components/HousePost.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-card
+    class="mx-auto"
+  >
+  <v-card-title>
+      <v-icon>mdi-account-circle-outline</v-icon>  {{userId}}
+    </v-card-title>
+    <v-img
+      :src="photoUrl"
+      height="200px"
+    ></v-img>
+
+    <v-card-title>
+      {{title}}
+    </v-card-title>
+
+    <v-card-subtitle>
+      {{subtitle}}
+    </v-card-subtitle>
+    <v-card-subtitle>
+      {{price}}
+    </v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+export default {
+    props:['userId', 'photoUrl', 'title', 'subtitle', 'price']
+}
+</script>
+
+<style>
+
+</style>
--- src/views/PostListView.vue
+++ src/views/PostListView.vue
@@ -1,13 +1,52 @@
 <template>
-  <h3>Post List</h3>
+<v-container grid-list-xs>
+  <v-row>
+    <v-col xs="12" md="4" lg="3" v-for="post in posts" :key="post.id">
+      <house-post
+        :userId="post.userId"
+        :photoUrl="post.photoUrl"
+        :title="post.title"
+        :subtitle="post.subtitle"
+        :price="post.price"
+      >
+      </house-post>
+    </v-col>
+  </v-row>
+</v-container>
 </template>
 
 <script>
+import HousePost from "../components/HousePost.vue";
 export default {
-
-}
+  components: {
+    HousePost,
+  },
+  mounted() {
+    const db = this.$firebase.firestore();
+    db.collection("posts")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          //   console.log(`${doc.id} => ${doc.data().title}`);
+          const post = {
+            id: doc.id,
+            userId: doc.data().userId,
+            photoUrl: doc.data().photoUrl,
+            title: doc.data().title,
+            subtitle: doc.data().subtitle,
+            price: doc.data().price,
+          };
+          this.posts.push(post);
+        });
+      });
+  },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
