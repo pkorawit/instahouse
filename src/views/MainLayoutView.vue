613a25f7bf1ad8d5ae2412b5439928383782a306
Korawit Prutsachainimmit <korawit.p@gmail.com>
2022-04-11 10:49:19 +0700

    Add main layout with navigation system

--- src/views/MainLayoutView.vue
+++ src/views/MainLayoutView.vue
@@ -1,19 +1,67 @@
 <template>
-<div>
-    <h3>Main Layout</h3>
+  <v-app>
     <div>
-        <router-view></router-view>
+      <v-app-bar color="indigo" dense dark>
+        <v-app-bar-nav-icon>
+          <v-icon>mdi-home-city-outline</v-icon></v-app-bar-nav-icon
+        >
+        InstaHouse
+        <v-spacer></v-spacer>
+        <v-menu left bottom>
+          <template v-slot:activator="{ attrs }">
+            <v-btn icon v-bind="attrs">
+              <v-icon>mdi-logout-variant</v-icon>
+            </v-btn>
+          </template>
+        </v-menu>
+      </v-app-bar>
     </div>
-</div>
-  
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+    <v-bottom-navigation
+      v-model="value"
+      :input-value="active"
+      color="indigo"
+      fixed
+    >
+      <v-btn to="/">
+        <span>Home</span>
+
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+
+      <v-btn to="favorite">
+        <span>Favorites</span>
+
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn to="mylist">
+        <span>My Listing</span>
+
+        <v-icon>mdi-clipboard-list-outline</v-icon>
+      </v-btn>
+      <v-btn to="about">
+        <span>About</span>
+
+        <v-icon>mdi-information-outline</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
 </template>
 
 <script>
 export default {
-
-}
+name: 'MainLyoutView',  
+  data() {
+    return {
+      value: 1,
+      active: true,
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
